9ddcab8bc10edd76d2b2e93fb45f5f1711e621e6
Vincent Yap <83166834+yapcentTV@users.noreply.github.com>
2024-11-28 13:20:30 +0800

    Add files via upload

---
+++ js/steal.js
@@ -0,0 +1,21 @@
+// $(document).ready(function() {
+// $('.link').click(function() { $(this).addClass('active'); });
+// $('#searchIcon').click(function() { $('.link').removeClass('active'); }); });
+
+document.addEventListener('contextmenu', event => {
+  event.preventDefault();
+});
+
+document.querySelectorAll('.disabled').forEach(element => {
+  element.style.pointerEvents = 'none';
+});
+
+$(document).keydown(function (event) {
+  if (event.keyCode == 123) { // Prevent F12
+      return false;
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I        
+      return false;
+  }
+});
+
+//
